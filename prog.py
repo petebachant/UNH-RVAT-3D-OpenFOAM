43375e9eb632ee98756018852af6fe9a388f6e43
Pete Bachant <petebachant@gmail.com>
2014-08-26 12:45:55 -0400

    Switch back to mesh7 parameters

--- prog.py
+++ prog.py
@@ -5,13 +5,7 @@ Created on Sat Sep  7 21:34:46 2013
 
 @author: pete
 """
-import sys
+
 import foampy
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == "-g":
-        gui = True
-else:
-    gui = False
-
-foampy.make_progress_bar(gui=gui)
+foampy.make_progress_bar()
