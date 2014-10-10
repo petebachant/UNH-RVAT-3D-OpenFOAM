3f3d05b1f16158f3a6b86f7867b5afc9c39cedd1
Pete Bachant <petebachant@gmail.com>
2014-10-09 23:07:05 -0400

    Upload script works

--- upload.py
+++ upload.py
@@ -10,6 +10,7 @@ token to do so.
 from __future__ import division, print_function
 from dropbox.client import DropboxClient
 from dropbox.rest import ErrorResponse
+import subprocess
 import json
 import os
 import re
@@ -18,36 +19,41 @@ def upload_file(filename):
     with open(filename, "rb") as f:
         response = client.put_file(os.path.join(dbdir, filename), f, 
                                    overwrite=True)
-    print(response)
 
 def get_token():
     with open(os.path.join(os.path.expanduser("~"), ".dropboxrc")) as f:
         token = json.load(f)["token"]
     return token
 
-def get_local_file_list():
+def get_local_dir_list():
     """Create list of local directories to compress and upload."""
     local_items = os.listdir("./")
-    items_to_upload = []
+    local_dir_list = []
     for f in local_items:
-        if re.match("^\d+", f) or re.match("^\d+\.\d+", f) or \
+        if re.match("^\d+$", f) or re.match("^\d+\.\d+$", f) or \
                 f in ["constant", "postProcessing"]:
-            items_to_upload.append(f)
+            if os.path.isdir(f):
+                local_dir_list.append(f)
+    return local_dir_list
 
-def get_dropbox_contents(client):
+def get_dropbox_contents(client, dbdir):
     try:
         folder_metadata = client.metadata(dbdir)
         dbcontents = folder_metadata["contents"]
-    except ErrorResponse, e:
+    except ErrorResponse:
         dbcontents = []
     return dbcontents
+
+def get_dropbox_filelist(client, dbdir):
+    """Returns list of tuples of filenames and sizes."""
+    dbcontents = get_dropbox_contents(client, dbdir)
+    dbfilelist = []
+    for f in dbcontents:
+        dbfilelist.append(str(f["path"].split("/")[-1]))
+    return dbfilelist
         
-uploaded_files = []
-for f in dbcontents:
-    print(f["size"])
-    uploaded_files.append(str(f["path"].split("/")[-1]))
-    
-print(uploaded_files)
+def compress_dir(dir):
+    subprocess.call(["tar", "-czvf", dir+".gz", dir])
         
 if __name__ == "__main__":
     # Call the case the subfolder where this script is located
@@ -58,3 +64,20 @@ if __name__ == "__main__":
     # Create Dropbox client    
     token = get_token()    
     client = DropboxClient(token)
+    
+    # Create list of local directories
+    local_dirs = get_local_dir_list()
+    local_files = [d + ".gz" for d in local_dirs]
+    
+    # Create list of files on Dropbox
+    db_files = get_dropbox_filelist(client, dbdir)
+    
+    for d in local_dirs:
+        f = d + ".gz"
+        if not f in db_files:
+            print("Compressing '{}'...".format(d))
+            compress_dir(d)
+            print("Uploading '{}'...".format(f))
+            upload_file(f)
+        else:
+            print("'{}' already uploaded".format(f))
