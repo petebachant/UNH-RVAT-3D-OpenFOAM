4251b67378f5cdb2770e8e7d607edb8171d60496
Pete Bachant <petebachant@gmail.com>
2015-03-17 18:26:20 -0400

    Use enumerate when looping through z/H values

--- processing.py
+++ processing.py
@@ -80,6 +80,9 @@ def calc_perf(theta_0=360, plot=False, verbose=True, inertial=False):
         return {"C_P" : "nan", 
                 "C_D" : "nan", 
                 "TSR" : "nan"}
+
+def plot_perf():
+    calc_perf(plot=True)
     
 def loadwake(time):
     """Loads wake data and returns y/R and statistics."""
@@ -118,12 +121,12 @@ def calcwake(t1=0.0, save=False):
     for n, t_i in enumerate(t):
         print("Loading sampled velocity data for t = {}".format(t_i))
         data = loadwake(t[n])
-        for m in range(len(z_H)):
-            u[n,m,:] = data[z_H[m]][1]
-            v[n,m,:] = data[z_H[m]][2]
-            w[n,m,:] = data[z_H[m]][3]
+        for m, z_H_i in enumerate(z_H):
+            u[n,m,:] = data[z_H_i][1]
+            v[n,m,:] = data[z_H_i][2]
+            w[n,m,:] = data[z_H_i][3]
             try:
-                xvorticity[n,m,:] = data[z_H[m]][4]
+                xvorticity[n,m,:] = data[z_H_i][4]
             except IndexError:
                 pass
     meanu = u.mean(axis=0)
