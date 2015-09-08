0df989573b648c10c6285a5cbecdd13bc1c78a09
Pete Bachant <petebachant@gmail.com>
2015-09-08 14:43:57 -0400

    Add function for plotting k contours

--- modules/plotting.py
+++ modules/plotting.py
@@ -57,19 +57,19 @@ def plot_k_profile(z_H=0.0, amount="total", newfig=True, save=False):
         plt.savefig(os.path.join(savedir, "k_{}_profile_{}_SA{}".format(
                 amount, z_H, savetype)))
 
-def plot_turb_lines(half=False):
+def plot_turb_lines(half=False, color="gray"):
     if half:
         plt.hlines(0.5, -1, 1, linestyles="solid", linewidth=2)
         plt.vlines(-1, 0, 0.5, linestyles="solid", linewidth=2)
         plt.vlines(1, 0, 0.5, linestyles="solid", linewidth=2)
     else:
-        plt.hlines(0.5, -1, 1, linestyles="solid", colors="gray",
+        plt.hlines(0.5, -1, 1, linestyles="solid", colors=color,
                    linewidth=3)
-        plt.hlines(-0.5, -1, 1, linestyles="solid", colors="gray",
+        plt.hlines(-0.5, -1, 1, linestyles="solid", colors=color,
                    linewidth=3)
-        plt.vlines(-1, -0.5, 0.5, linestyles="solid", colors="gray",
+        plt.vlines(-1, -0.5, 0.5, linestyles="solid", colors=color,
                    linewidth=3)
-        plt.vlines(1, -0.5, 0.5, linestyles="solid", colors="gray",
+        plt.vlines(1, -0.5, 0.5, linestyles="solid", colors=color,
                    linewidth=3)
                    
 def plot_exp_lines():
@@ -133,6 +133,31 @@ def plot_meancontquiv(save=False, show=False, savetype=".pdf",
         plt.show()
     if save:
         plt.savefig("figures/meancontquiv"+savetype)
+        
+def plot_kcont(cb_orientation="vertical", newfig=True):
+    """Plot contours of TKE."""
+    k = load_k_map()
+    y_R = np.round(np.asarray(k.columns.values, dtype=float), decimals=4)
+    z_H = np.asarray(k.index.values, dtype=float)
+    if newfig:
+        plt.figure(figsize=(7.5, 1.9))
+    cs = plt.contourf(y_R, z_H, k, 20, cmap=plt.cm.coolwarm,
+                      levels=np.linspace(0, 0.09, num=19))
+    plt.xlabel(r"$y/R$")
+    plt.ylabel(r"$z/H$")
+    if cb_orientation == "horizontal":
+        cb = plt.colorbar(cs, shrink=1, extend="both", 
+                          orientation="horizontal", pad=0.3)
+    elif cb_orientation == "vertical":
+        cb = plt.colorbar(cs, shrink=1, extend="both", 
+                          orientation="vertical", pad=0.02)
+    cb.set_label(r"$k/U_\infty^2$")
+    plot_turb_lines(color="black")
+    plt.ylim((0, 0.63))
+    ax = plt.axes()
+    ax.set_aspect(2)
+    plt.yticks([0,0.13,0.25,0.38,0.5,0.63])
+    plt.tight_layout()
 
 
 if __name__ == "__main__":
